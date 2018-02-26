141da193050fe2b82972a607bf1095b5a93afde9
Medi Assumani <mediassumani49@gmail.com>
2018-02-26 18:32:31 -0500

    implementing tableViews

--- BookGuru/BookGuru/Controllers/BookListViewController.swift
+++ BookGuru/BookGuru/Controllers/BookListViewController.swift
@@ -6,7 +6,29 @@
 //  Copyright © 2018 Assumani, Medi. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreData
 
+class BookListViewController: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    // - MARK : UITableView Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookListTableViewCell", for: indexPath)
+        
+        return cell
+    }
+ 
+}
---
+++ BookGuru/BookGuru/Views/BookListTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookListTableViewCell.swift
+//  BookGuru
+//
+//  Created by Assumani, Medi on 2/26/18.
+//  Copyright © 2018 Assumani, Medi. All rights reserved.
+//
+
+import UIKit
+
+class BookListTableViewCell : UITableViewCell{
+    
+}
