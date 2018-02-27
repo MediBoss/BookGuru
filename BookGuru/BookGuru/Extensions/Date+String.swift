c53b074584e3e2a6c1b9cab0c086ff116003e62b
Medi Assumani <mediassumani49@gmail.com>
2018-02-27 17:55:47 -0500

    adding extensions

--- BookGuru/BookGuru/Extensions/Date+String.swift
+++ BookGuru/BookGuru/Extensions/Date+String.swift
@@ -8,11 +8,19 @@
 
 import Foundation
 
+// Extending the Date Type functionality to be able to convert date format to string style
 extension Date{
     func dateToStringConversion() -> String{
         return DateFormatter.localizedString(from: self, dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.medium)
     }
-    func dateToStringConversion() -> String{
         
+  }
+
+// Extending the NSDate Type functionality to be able to convert date format to string style
+
+extension NSDate{
+    func dateToStringconversion() -> String{
+        return DateFormatter.localizedString(from: (self as Date), dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.medium)
     }
 }
+
